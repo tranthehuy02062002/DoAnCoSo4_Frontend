395a8bcf728071db26cef58ca37e19bac047e7cd
NguyenMinhPhuong23 <phuongminhnguyen123a@gmail.com>
2024-07-08 16:19:25 +0700

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import ProductCard from "./Components/Product/ProductCard";
+import Product from "./Components/Product/Product";
 
 const App = () => {
-  return <div>Hello world!!</div>;
+  return <div><Product/></div>;
 };
 
 export default App;
